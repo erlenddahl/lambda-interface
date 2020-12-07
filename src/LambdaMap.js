ca7615889b1425c2bae1cdaf81135030d4a76af3
Erlend Dahl <erlend.dahl@sintef.no>
2020-12-07 11:00:57 +0100

    Restructured as React component.

--- src/LambdaMap.js
+++ src/LambdaMap.js
@@ -22,8 +22,6 @@ const TERRAIN_IMAGE = `https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.
 const SURFACE_IMAGE = `https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.png?access_token=${MAPBOX_TOKEN}`;
 
 // https://docs.mapbox.com/help/troubleshooting/access-elevation-data/#mapbox-terrain-rgb
-// Note - the elevation rendered by this example is greatly exagerated!
-// Looks like it should be 65536, 256 and 1 for actual elevation.
 const ELEVATION_DECODER = {
   rScaler: 6553.6,
   gScaler: 25.6,
@@ -31,52 +29,56 @@ const ELEVATION_DECODER = {
   offset: -10000
 };
 
-export default function App({texture = SURFACE_IMAGE, wireframe = false, initialViewState = INITIAL_VIEW_STATE}){
-  const layer = new TerrainLayer({ //eslint-disable-line
-    id: 'terrain',
-    minZoom: 0,
-    maxZoom: 23,
-    strategy: 'no-overlap',
-    elevationDecoder: ELEVATION_DECODER,
-    elevationData: TERRAIN_IMAGE,
-    texture,
-    wireframe,
-    color: [255, 255, 255]
-  });
+class LambdaMap extends React.Component{
+  render(){
 
-  const sites = [
-    {
-      id: 0,
-      name: "Test",
-      color: {
-        r: 160,
-        g: 0,
-        b: 0,
-        a: 255
-      },
-      latLng: [63.42050427064208, 10.355430273040675]
-    }
-  ];
+    const sites = [
+      {
+        id: 0,
+        name: "Test",
+        color: {
+          r: 160,
+          g: 0,
+          b: 0,
+          a: 255
+        },
+        latLng: [63.42050427064208, 10.355430273040675]
+      }
+    ];
+  
+    const data = {
+      "type":"FeatureCollection",
+      "features": sites.map(p => ({"type":"Feature","properties": p,"geometry":{"type":"Polygon","coordinates":circle([p.latLng[1], p.latLng[0]], 0.01).geometry.coordinates}}))
+    };
 
-  const data = {
-    "type":"FeatureCollection",
-    "features": sites.map(p => ({"type":"Feature","properties": p,"geometry":{"type":"Polygon","coordinates":circle([p.latLng[1], p.latLng[0]], 0.01).geometry.coordinates}}))
-  };
-
-  const geoJsonLayer = new GeoJsonLayer({ //eslint-disable-line
-    id: 'geojson-layer',
-    data,
-    pickable: true,
-    stroked: false,
-    filled: true,
-    extruded: true,
-    lineWidthScale: 20,
-    lineWidthMinPixels: 2,
-    getFillColor: p => { var c = p.properties.color || {}; return [c.r || 0, c.g || 0, c.b || 0, c.a || 255]; },
-    getRadius: 10,
-    getLineWidth: 1,
-    getElevation: 300
-  });
-
-  return <DeckGL initialViewState={initialViewState} controller={true} layers={[layer, geoJsonLayer]} />;
+    return <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={[
+      new TerrainLayer({
+        id: 'terrain',
+        minZoom: 0,
+        maxZoom: 23,
+        strategy: 'no-overlap',
+        elevationDecoder: ELEVATION_DECODER,
+        elevationData: TERRAIN_IMAGE,
+        texture: SURFACE_IMAGE,
+        wireframe: false,
+        color: [255, 255, 255]
+      }),
+      new GeoJsonLayer({
+        id: 'geojson-layer',
+        data,
+        pickable: true,
+        stroked: false,
+        filled: true,
+        extruded: true,
+        lineWidthScale: 20,
+        lineWidthMinPixels: 2,
+        getFillColor: p => { var c = p.properties.color || {}; return [c.r || 0, c.g || 0, c.b || 0, c.a || 255]; },
+        getRadius: 10,
+        getLineWidth: 1,
+        getElevation: 300
+      })
+    ]} />;
+  }
 }
+
+export default LambdaMap;
