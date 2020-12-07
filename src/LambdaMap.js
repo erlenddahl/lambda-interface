08a5354a2c432519de67c343c801ddfe19e25ef8
Erlend Dahl <erlend.dahl@sintef.no>
2020-12-07 15:40:41 +0100

    Fixed mouseover coloring.

--- src/LambdaMap.js
+++ src/LambdaMap.js
@@ -30,11 +30,17 @@ const ELEVATION_DECODER = {
 };
 
 class LambdaMap extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = { hoveredObject: null };
+  }
+
   render(){
 
-    const sites = [
+    const stations = [
       {
-        id: 0,
+        id: 1254,
         name: "Test",
         color: {
           r: 160,
@@ -42,42 +48,52 @@ class LambdaMap extends React.Component{
           b: 0,
           a: 255
         },
-        latLng: [63.42050427064208, 10.355430273040675]
+        latLng: [63.42050427064208, 10.355430273040675],
+        frequency: 22000,
+        height: 300
       }
     ];
   
     const data = {
       "type":"FeatureCollection",
-      "features": sites.map(p => ({"type":"Feature","properties": p,"geometry":{"type":"Polygon","coordinates":circle([p.latLng[1], p.latLng[0]], 0.01).geometry.coordinates}}))
+      "features": stations.map(p => ({"type":"Feature","properties": p,"geometry":{"type":"Polygon","coordinates":circle([p.latLng[1], p.latLng[0]], 0.01).geometry.coordinates}}))
     };
 
-    return <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={[
-      new TerrainLayer({
-        id: 'terrain',
-        minZoom: 0,
-        maxZoom: 23,
-        strategy: 'no-overlap',
-        elevationDecoder: ELEVATION_DECODER,
-        elevationData: TERRAIN_IMAGE,
-        texture: SURFACE_IMAGE,
-        wireframe: false,
-        color: [255, 255, 255]
-      }),
-      new GeoJsonLayer({
-        id: 'geojson-layer',
-        data,
-        pickable: true,
-        stroked: false,
-        filled: true,
-        extruded: true,
-        lineWidthScale: 20,
-        lineWidthMinPixels: 2,
-        getFillColor: p => { var c = p.properties.color || {}; return [c.r || 0, c.g || 0, c.b || 0, c.a || 255]; },
-        getRadius: 10,
-        getLineWidth: 1,
-        getElevation: 300
-      })
-    ]} />;
+    return <DeckGL 
+      initialViewState={INITIAL_VIEW_STATE} 
+      controller={true} 
+      layers={[
+        new TerrainLayer({
+          id: 'terrain',
+          minZoom: 0,
+          maxZoom: 23,
+          strategy: 'no-overlap',
+          elevationDecoder: ELEVATION_DECODER,
+          elevationData: TERRAIN_IMAGE,
+          texture: SURFACE_IMAGE,
+          wireframe: false,
+          color: [255, 255, 255]
+        }),
+        new GeoJsonLayer({
+          id: 'geojson-layer',
+          data,
+          pickable: true,
+          stroked: false,
+          filled: true,
+          extruded: true,
+          lineWidthScale: 20,
+          lineWidthMinPixels: 2,
+          getFillColor: p => { console.log("COLOR", p); var c = p.properties.color || {}; if(p.properties.isSelected){ c = [0,0,0,255]; } return [c.r || 0, c.g || 0, c.b || 0, c.a || 255]; },
+          getRadius: 10,
+          getLineWidth: 1,
+          getElevation: p => p.properties.height,
+          autoHighlight: true,
+          highlightColor: [0,0,0,255],
+          onClick: info => console.log("CLICK", info)
+        })
+    ]}
+    getTooltip={function(info){return info.object && info.object.properties.name; }}>
+    </DeckGL>;
   }
 }
 
