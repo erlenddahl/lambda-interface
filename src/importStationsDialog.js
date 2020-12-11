3f5483db5e97a0f6b40fecaab8b48470122e825f
Erlend Dahl <erlend.dahl@sintef.no>
2020-12-11 18:19:55 +0100

    Added working onChange event handler for the import form.

--- src/importStationsDialog.js
+++ src/importStationsDialog.js
@@ -8,12 +8,15 @@ import PropTypes from 'prop-types';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
+import { WithOnChangeHandler } from 'formik-form-callbacks';
+
 class ImportStationsDialog extends React.Component {
 
     constructor(props) {
         super(props);
 
         this.onSave = this.onSave.bind(this);
+        this.onFormValueChanged = this.onFormValueChanged.bind(this);
 
         this.validation = yup.object().shape({
             separator: yup.string().required(),
@@ -36,6 +39,25 @@ class ImportStationsDialog extends React.Component {
     onSave(values, { setSubmitting }) {
         setSubmitting(false);
         this.props.onSave(values);
+    }
+
+    onFormValueChanged(values, errors){
+
+        if(this.formValues && this.formValues.timer){
+            clearTimeout(this.formValues.timer);
+        } 
+
+        this.formValues = {
+            values, 
+            errors, 
+            hasErrors: Object.keys(errors).length > 0,
+            timer: setTimeout(() => {
+                if(!this.formValues.hasErrors){
+                    // Now, here is an actual valid form change.
+                    console.log(this.formValues.values);
+                }
+            }, 500)
+        };
     }
 
     render() {
@@ -69,7 +91,7 @@ class ImportStationsDialog extends React.Component {
                         <Field name="hasHeaders">
                             {({ field }) => (
                                 <FormGroup controlId="hasHeaders">
-                                    <FormCheck label="Has headers" type="checkbox" {...field} />
+                                    <FormCheck label="Has headers" type="checkbox" {...field} checked={field.value} />
                                     <ErrorMessage name="hasHeaders" component="div" />
                                 </FormGroup>
                             )}
@@ -83,6 +105,11 @@ class ImportStationsDialog extends React.Component {
                                 </FormGroup>
                             )}
                         </Field>
+
+                        <WithOnChangeHandler>
+                            {({ values, errors }) => this.onFormValueChanged(values, errors)}
+                        </WithOnChangeHandler>
+
                         <Button disabled={isSubmitting} type="submit">Import</Button>
                         <Button variant="secondary" disabled={isSubmitting} onClick={this.props.onCancel}>Cancel</Button>
                     </Form>
