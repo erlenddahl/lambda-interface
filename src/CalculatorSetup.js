80c0714677be636eeefdb9b7b68ba1a6b734af35
Erlend Dahl <erlend.dahl@gmail.com>
2021-08-11 15:51:55 +0200

    Improved job status view.

--- src/CalculatorSetup.js
+++ src/CalculatorSetup.js
@@ -32,16 +32,18 @@ class CalculatorSetup extends React.Component {
     async refreshJobStatuses() {
         try{
             await Promise.all(this.state.jobs.map(async p => {
+
+                if(p.status == "Failed" || p.status == "Finished") return;
+
                 const requestOptions = {
                     method: 'GET',
                     headers: { 'Content-Type': 'application/json' }
                 };
                 const response = await fetch(this.apiUrl + "?key=" + p.data.Id, requestOptions);
                 const data = await response.json();
+                if(!data.data) return;
                 
-                this.setState(state => ({
-                    jobs: state.jobs.filter(p => p.data.id !== data.data.id).concat(data)
-                }));
+                this.updateJobState(data);
             }));
         }catch(ex){
             console.log(ex);
@@ -49,10 +51,15 @@ class CalculatorSetup extends React.Component {
         this.intervalID = setTimeout(this.refreshJobStatuses.bind(this), 1000);
     }
 
+    updateJobState(data){
+        this.setState(state => ({
+            jobs: state.jobs.filter(p => p.data.Id !== data.data.Id).concat(data).sort((a,b) => a.data.Enqueued.localeCompare(b.data.Enqueued)),
+            showDetails: !state.showDetails || state.showDetails.data.Id !== data.data.Id ? state.showDetails : data
+        }));
+    }
+
     async onCalculationClicked(){
         this.setState({ isBusy: true });
-
-        console.log(this.props.selectedStations);
 
         const wgs84 = "+proj=longlat +datum=WGS84 +no_defs";
         const utm33 = "+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs"; //EPSG:32633
@@ -79,10 +86,8 @@ class CalculatorSetup extends React.Component {
             };
             const response = await fetch(this.apiUrl, requestOptions);
             const data = await response.json();
-            
-            this.setState(state => ({
-                jobs: state.jobs.filter(p => p.data.id !== data.data.id).concat(data)
-            }));
+
+            this.updateJobState(data);
 
         }catch(ex){
             console.log(ex);
@@ -96,16 +101,33 @@ class CalculatorSetup extends React.Component {
             <Alert variant="info">{this.props.selectedStations.length} stations selected for calculations.</Alert>
             <Button onClick={this.onCalculationClicked} disabled={this.state.isBusy || !this.props.selectedStations.length}>Calculate</Button>
 
-            {this.state.jobs.map(p =>
-                <div key={p.data.Id}>
-                    <span>Job created: {moment(p.data.Enqueued).format('MMMM Do YYYY, HH:mm:ss')}</span><br />
-                    <span>Current status: {p.status}</span>
-                    {p.data.RunException && 
-                        <Alert variant="danger">{p.data.RunException}</Alert>
-                    }
-                    <ConsoleInformationPanel data={p.data.Snapshot}></ConsoleInformationPanel>
-                </div>
-            )}
+            {this.state.jobs.length > 0 && (<table style={{width: "100%"}}>
+                <thead>
+                    <tr>
+                        <th>Created</th>
+                        <th>Status</th>
+                        <th>Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.state.jobs.map(p =>
+                    <React.Fragment key={p.data.Id}>
+                        <tr>
+                            <td>{moment(p.data.Enqueued).format('MMMM Do YYYY, HH:mm:ss')}</td>
+                            <td>{p.status}</td>
+                            <td>{p.status != "InQueue" && (<Button onClick={() => this.setState({showDetails: p})}>Show details</Button>)}</td>
+                        </tr>
+                    </React.Fragment>
+                )}
+                </tbody>
+            </table>)}
+
+            {this.state.showDetails != null && (<div>
+                {this.state.showDetails.data.RunException && 
+                    <Alert variant="danger">{this.state.showDetails.data.RunException}</Alert>
+                }
+                <ConsoleInformationPanel data={this.state.showDetails.data.Snapshot}></ConsoleInformationPanel>
+            </div>)}
         </div>
     }
 }
