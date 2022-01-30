a14d69ff23ec0b30868ad5e5d9a72fa9a4e3e62f
Erlend Dahl <erlend.dahl@gmail.com>
2022-01-30 14:37:05 +0100

    Removed station info dialog; edit combined with context menu has everything we need.

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -14,8 +14,10 @@ class ContextMenu extends React.Component {
         return <div className="map-context-menu" style={{ left: this.props.left, top: this.props.top }}>
             WGS84: {this.props.coordinate[0].toFixed(8) + ", " + this.props.coordinate[1].toFixed(8)}
             
-            <Button className="mt-4 w-100" onClick={() => this.props.onNewStationRequested(this.props.coordinate)}>Create new station here</Button>
-            {this.props.selectedStation && <Button className="w-100" onClick={() => this.props.onNewStationRequested(this.props.coordinate)}>Calculate path loss from {this.props.selectedStation.name}</Button>}
+            {!this.props.station && <Button variant="success" className="mt-4 w-100" onClick={() => this.props.onNewStationRequested(this.props.coordinate)}>Create new station at this location</Button>}
+            {this.props.station && <Button className="mt-2 w-100" onClick={() => this.props.onMoveStationRequested()}>Move station to this location</Button>}
+            {this.props.station && this.props.station.isEditClone && <Button className="mt-2 w-100" onClick={() => this.props.onCalculationRequested(this.props.coordinate)}>Calculate path loss from {this.props.station.name}</Button>}
+            <Button variant="secondary" className="mt-2 w-100" onClick={() => this.props.onHideMenuRequested()}>Hide menu</Button>
         </div>
     }
 }
@@ -25,8 +27,11 @@ ContextMenu.propTypes = {
     left: PropTypes.number,
     top: PropTypes.number,
     coordinate: PropTypes.array,
-    selectedStation: PropTypes.object,
+    station: PropTypes.object,
     onNewStationRequested: PropTypes.func,
+    onMoveStationRequested: PropTypes.func,
+    onCalculationRequested: PropTypes.func,
+    onHideMenuRequested: PropTypes.func
 };
 
 export default ContextMenu;
